74481470d0313b4f5b4d00cefff06b9dd8abcbdf
Jun Nishimura <n.junjun0303@gmail.com>
2023-03-15 19:33:47 +0900

    add abc/289/c

--- ABC/289/c.cpp
+++ ABC/289/c.cpp
@@ -2,4 +2,32 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(m);
+    for (int i = 0; i < m; i++) {
+        int c; cin >> c;
+        for (int j = 0; j < c; j++) {
+            int t; cin >> t;
+            a[i].push_back(t);
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i <= pow(2, m); i++) {
+        set<int> s;
+        for (int j = 0; j < m; j++) {
+            if ((1<<j & i) > 0) {
+                for (auto x : a[j]) {
+                    s.insert(x);
+                }
+            }
+        }
+        bool ok = true;
+        for (int i = 1; i <= n; i++) {
+            if (s.count(i) == 0) ok = false;
+        }
+        if (ok) ans++;
+    }
+    
+    cout << ans << endl;
 }
