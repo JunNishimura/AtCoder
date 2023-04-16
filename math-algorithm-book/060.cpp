301d00114cb03f2d6ac7df3223798b7003cbd2c7
Jun Nishimura <n.junjun0303@gmail.com>
2023-04-16 15:26:03 +0900

    add 060

---
+++ math-algorithm-book/060.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    if (n % 4 == 0) {
+        cout << "Second" << endl;
+    } else {
+        cout << "First" << endl;
+    }
+}
