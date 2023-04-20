719853e0b2ae0e103dc79cca7227ec845ee8d208
Jun Nishimura <n.junjun0303@gmail.com>
2023-04-20 22:04:49 +0900

    add 065

---
+++ math-algorithm-book/065.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long h, w;
+    cin >> h >> w;
+    long long ans = 0;
+    if (h == 1 || w == 1) ans = 1;
+    else ans = (h * w + 1) / 2;
+    cout << ans << endl;
+}
