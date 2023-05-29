498dfdcef1da838c7c6df77f85a84d282919c69a
Jun Nishimura <n.junjun0303@gmail.com>
2023-05-29 22:23:36 +0900

    update 082

--- math-algorithm-book/082.cpp
+++ math-algorithm-book/082.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int l[300000], r[300000];
+struct Movie {
+    int l, r;
+};
+
+bool operator<(const Movie &a1, const Movie &a2) {
+    if (a1.r < a2.r) return true;
+    if (a1.r > a2.r) return false;
+    if (a1.l < a2.l) return true;
+    return false;
+}
+
+Movie A[300001];
 
 int main() {
     int n; cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> l[i] >> r[i];
+        cin >> A[i].l >> A[i].r;
     }
+    sort(A, A+n);
 
     int ans = 0;
     int p = 0;
-    while (p <= 86400) {
-        int s = 86401;
-        for (int i = 0; i < n; i++) {
-            if (l[i] >= p && r[i] < s) {
-                s = r[i];
-            }
+    for (int i = 0; i < n; i++) {
+        if (A[i].l >= p) {
+            p = A[i].r;
+            ans++;
         }
-        if (s == 86401) break;
-        ans++;
-        p = s;
     }
     cout << ans << endl;
 }
